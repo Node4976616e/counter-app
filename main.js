501c0a0dbcab734566683fed9b6afe2c02579d54
Ivan Poch <45315570+Node4976616e@users.noreply.github.com>
2023-01-21 23:25:03 +0000

    Added comments and changed innerText for textContent

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let count = 0;
 let saved = document.getElementById("saved-progress");
 
+//Function for incrementing counter by one on click
 function increment() {
     count += 1;
-    document.getElementById("counter").innerText = count;
+    document.getElementById("counter").textContent = count;
     console.log(count);
 }
-
+//Function for decrementing counter by one on click
 function decrement() {
     if (count > 0) {
         count -= 1;
@@ -14,20 +15,21 @@ function decrement() {
         count = 0;
         alert("Can't have negative value!")
     }
-    document.getElementById("counter").innerText = count;
+    document.getElementById("counter").textContent = count;
     console.log(count);
 }
 
+//Function for reseting current count
 function resetCount() {
     if (count > 0) {
         count = 0;
-        document.getElementById("counter").innerText = count;
+        document.getElementById("counter").textContent = count;
     }
     else {
         alert("Can't reset counter, it is already at 0!")
     }
 }
-
+//Function for saving current count on a side into <ul> as new <li> for each click on save
 function saveCount() {
     if (count < 1) {
         alert("Nothing to save!");
@@ -38,7 +40,7 @@ function saveCount() {
     }
 
 }
-
+//Function for clearing all saved entries, will delete all tags inside <ul>
 function clearSaved() {
     saved.innerHTML = "";
 }
