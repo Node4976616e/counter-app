c593b77954bacec6cb2dd2bb22db44edcf07be6c
Ivan Poch <45315570+Node4976616e@users.noreply.github.com>
2023-01-21 22:19:13 +0000

    Added decrement button and alert messages

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ let count = 0;
 function increment () {
     count += 1;
     document.getElementById("counter").innerText = count;
+    console.log(count);
+}
+
+function decrement () {
+    if (count > 0) {
+        count -= 1;
+    } else if (count == 0) {
+        count = 0;
+        alert("Can't have negative value!")
+    }
+    document.getElementById("counter").innerText = count;
+    console.log(count);
 }
 
 function resetCount () {
